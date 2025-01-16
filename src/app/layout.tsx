f82b31d02afd40e37853e4e7f4fe0e781636211c
simbacitydev <simbacity.dev@gmail.com>
2025-01-16 10:30:37 +0100

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import "@/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+import { TRPCReactProvider } from "@/trpc/react";
+
+export const metadata: Metadata = {
+  title: "Simba City",
+  description: "",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body className="dark">
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
+    </html>
+  );
+}
